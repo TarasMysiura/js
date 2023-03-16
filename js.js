aca639c625a13abdd8a8967e11e69b723eafd3e5
TarasMysiura <tarasmysiura@gmail.com>
2023-03-16 18:03:20 +0200

    +

--- js.js
+++ js.js
@@ -192,19 +192,73 @@
 // console.log(keys);
 // console.log(values);
 // !===============================================
-function countProps(object) {
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+//   const keys = Object.keys(object);
+//   for (const key of keys) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+// console.log(countProps({ name: "Mango", age: 2 }));
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+// !==================================================
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   for (const key of Object.keys(salaries)) {
+//     totalSalary += salaries[key];
+//     }
+//   // Change code above this line
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+// !===================================
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+// !===================================
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
   // Change code below this line
-  let propCount = 0;
-
-  const keys = Object.keys(object);
-  for (const key of keys) {
-    if (object.hasOwnProperty(key)) {
-      propCount += 1;
-    }
+for (const product of products) {
+  if (product.name === productName) {
+    return product.price;
   }
+}
+return null;
 
-  return propCount;
   // Change code above this line
 }
-console.log(countProps({ name: "Mango", age: 2 }));
-console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+console.log(getProductPrice("Radar"));
+console.log(getProductPrice("Grip"));
+console.log(getProductPrice("Scanner"));
+console.log(getProductPrice("Droid"));
+console.log(getProductPrice("Engine"));
