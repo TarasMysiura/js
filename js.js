930afadae8bfb625aec103199b86f69e2eaf82c9
TarasMysiura <tarasmysiura@gmail.com>
2023-03-18 11:19:03 +0200

    +

--- js.js
+++ js.js
@@ -380,24 +380,26 @@
 // console.log(lowTomorrow);
 // console.log(tomorrowIcon);
 // !============================26===========================
-// Change code below this line
-function calculateMeanTemperature(forecast) {
-  const {
-    today: { low: todayLow, high: todayHigh },
-    tomorrow: { low: tomorrowLow, high: tomorrowHigh },
-  } = forecast;
+// // Change code below this line
+// function calculateMeanTemperature(forecast) {
+//   const {
+//     today: { low: todayLow, high: todayHigh },
+//     tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+//   } = forecast;
   
-  // Change code above this line
-  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
-}
-console.log(
-  calculateMeanTemperature({
-    today: { low: 28, high: 32 },
-    tomorrow: { low: 25, high: 29 },
-  })
-);
-console.log(calculateMeanTemperature({
-  today: { low: 37, high: 40 },
-  tomorrow: { low: 33, high: 38 },
-})
-);
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+// console.log(
+//   calculateMeanTemperature({
+//     today: { low: 28, high: 32 },
+//     tomorrow: { low: 25, high: 29 },
+//   })
+// );
+// console.log(calculateMeanTemperature({
+//   today: { low: 37, high: 40 },
+//   tomorrow: { low: 33, high: 38 },
+// })
+// );
+// !=======================27=============================
+
