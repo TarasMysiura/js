f902751797f9c1a141a90397fd090aff83a524bb
TarasMysiura <tarasmysiura@gmail.com>
2023-03-19 17:31:57 +0200

    Update js.js

--- js.js
+++ js.js
@@ -457,19 +457,22 @@
 // console.log(makeTask({ text: "Buy bread" }));
 
 // !==========================31================================
-// Change code below this line
-function add(...args) {
-    let total = 0
-    for (const arg of args) {
-      total += arg
-    }
-    return total
-    // Change code above this line
-}
-console.log(add(15, 27));
-console.log(add(12, 4, 11, 48));
-console.log(add(32, 6, 13, 19, 8));
-console.log(add(74, 11, 62, 46, 12, 36));
+// // Change code below this line
+// function add(...args) {
+//     let total = 0
+//     for (const arg of args) {
+//       total += arg
+//     }
+//     return total
+//     // Change code above this line
+// }
+// console.log(add(15, 27));
+// console.log(add(12, 4, 11, 48));
+// console.log(add(32, 6, 13, 19, 8));
+// console.log(add(74, 11, 62, 46, 12, 36));
+
+// !============================32===============================
+
 
 
 
