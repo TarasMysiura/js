5146289f0c95aac881841854f5ac94b0e30c44c2
TarasMysiura <tarasmysiura@gmail.com>
2023-04-17 11:12:16 +0300

    Update calc_9a.py

--- calc_9a.py
+++ calc_9a.py
@@ -2,13 +2,23 @@ a = float(input("Введіть число a: "))
 action = input("Введіть дію: ")
 b = float(input("Введіть число b: "))
 print()
-if (action == "+"):
-  print("Сума чисел дорівнює " + str(a + b))
-elif (action == "-"):
-  print("Різниця чисел дорівнює " + str(a - b))
-elif (action == "*"):
-  print("Добуток чисел дорівнює " + str(a * b))
-elif (action == "/"):
-  print("Частка чисел дорівнює " + str(a / b))
-else:
-  print("Не правильна дія")
+
+# match action:
+#     case "+":
+#       print("Сума чисел дорівнює " + str(a + b))
+#     case "-":
+#       print("Різниця чисел дорівнює " + str(a - b))
+#     case "*":
+#       print("Добуток чисел дорівнює " + str(a * b))
+
+#     case "/":
+#       print("Частка чисел дорівнює " + str(a / b))
+        
+#     case default:
+#       print("Не правильна дія")
+
+rez = ("Сума чисел дорівнює " + \
+    str(a + b)) if (action == "+") else "Не правильна дія"
+
+print(rez)
+
