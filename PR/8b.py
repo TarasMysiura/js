90602941f4926a890445f23787eacbcd0a043e86
TarasMysiura <tarasmysiura@gmail.com>
2023-05-14 21:04:45 +0300

    Update 8b.py

--- PR/8b.py
+++ PR/8b.py
@@ -27,13 +27,36 @@
 # якщо вони повертатимуть списані зошити й на отримані гроші купуватимуть чисті?
 
 
-s = 50
-new = 10
-full = 2
+# s = 50
+# new = 10
+# full = 2
 
-s1 = 0
-while (s1 <= s):
-    s1 = s1 + (new -full) * i
-    print(s1)
-    i = i +1
-print(i)
+# s1 = 0
+# while (s1 <= s):
+#     s1 = s1 + (new -full) * i
+#     print(s1)
+#     i = i +1
+# print(i)
+
+# a = 0
+
+# for a in range(1,10,4):
+#     print(a)
+# print()
+# print(a)
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+suma = 0
+j = 0
+for i in range(a,b):
+    suma += i  # suma = suma + i
+    j += 1
+    print("Suma = " + str(suma))
+    if (i == 4):
+        break
+print()
+print(j)
+print("Suma = " + str(suma))
+print("Avenge = " + str(suma / j))
